06d14f1bf1dd90d2260c13862e4b1bb0a8ce8588
VildeStabell <Vilde.voss.stabell@gmail.com>
2020-09-10 09:38:23 +0200

    #10 - Setup of the api-interaction in Installation component.

---
+++ exhibition/src/components/Installation/installation.tsx
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+
+interface InstallationState {
+    poem : [],
+    url : string,
+}
+
+interface InstallationProps {}
+
+class Installation extends React.Component<InstallationProps, InstallationState> {
+
+    constructor(props: InstallationProps, poetryUrl: string) {
+        super(props);
+        this.state = {poem : [], url: poetryUrl};
+    }
+
+    componentDidMount() {
+        fetch(this.state.url)
+            .then((response) => response.json())
+            .then(poem => poem[0].lines);
+    }
+
+    render() {
+        return (<div>
+                {this.state.poem.map((item) => <p>{item}</p>)}}
+            </div>
+        );
+    }
+}
+
+export default Installation;
