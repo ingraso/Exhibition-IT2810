a9e979af9cf5dacc6d1d3ff8e3f9f60510685513
Ingrid Amalie Solbjørg <ingraso@stud.ntnu.no>
2020-09-11 10:28:14 +0200

    Add Carousel component

--- exhibition/src/App.tsx
+++ exhibition/src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import Carousel from './components/Example/Carousel/carousel';
+import React from "react";
+import Carousel from "./components/Carousel/carousel";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-
-      <Carousel/>
+      <Carousel />
     </div>
   );
 }
