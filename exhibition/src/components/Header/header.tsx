a80fbe1872de5499660e197e6c49665815d9588b
Karolill <56150049+Karolill@users.noreply.github.com>
2020-09-17 15:13:42 +0200

    #5 - Added header, menu, and two buttons

---
+++ exhibition/src/components/Header/header.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.css';
+
+const header = () => {
+    return(
+        <section id="header">
+            <h1 id="title">Geometry gallery</ h1>
+        </section>
+    );
+}
+
+export default header;
---
+++ exhibition/src/components/Menu/menu.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import "./menu.css";
+
+/*
+A component for showing/hiding the menu. 
+The open-hook is used to decide if the menu is shown or not. 
+*/
+
+const Menu = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div>
+      <div id="menu" className={open ? "open" : "closed"}>
+        <div
+          id="close-button"
+          className={open ? "open" : "closed"}
+          onClick={() => setOpen(!open)}
+        >
+          <div className="line-one">
+            <div className="line-two"></div>
+          </div>
+        </div>
+      </div>
+      <div
+        id="burger-button"
+        className={open ? "open" : "closed"}
+        onClick={() => setOpen(!open)}
+      >
+        <div id="top-line" className={open ? "open" : "closed"}></div>
+        <div id="middle-line" className={open ? "open" : "closed"}></div>
+        <div id="bottom-line" className={open ? "open" : "closed"}></div>
+      </div>
+      <div id="blurry-area" className={open ? "open" : "closed"}></div>
+    </div>
+  );
+};
+
+export default Menu;
