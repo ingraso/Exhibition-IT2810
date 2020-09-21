783d6c09969ea9459d5d1edf9f85f5b5cfc89b3b
Karolill <56150049+Karolill@users.noreply.github.com>
2020-09-21 21:22:23 +0200

    #5 - cleaned the code for the menu

--- exhibition/src/components/Menu/menu.tsx
+++ exhibition/src/components/Menu/menu.tsx
@@ -6,40 +6,32 @@ A component for showing/hiding the menu.
 The open-hook is used to decide if the menu is shown or not. 
 */
 
-const Menu = ({ favOnClick } : { favOnClick: any }) => {
+const Menu = ({ favOnClick }: { favOnClick: any }) => {
   const [open, setOpen] = useState(false);
 
   return (
     <div>
       <div id="menu" className={open ? "open" : "closed"}>
-        <div
-          id="close-button"
-          className={open ? "open" : "closed"}
-          onClick={() => setOpen(!open)}
-        >
+        <button id="displayFavButton" onClick={favOnClick} type="button">
+          Only display favorites
+        </button>
+        <div id="close-button" onClick={() => setOpen(!open)}>
           <div className="line-one">
             <div className="line-two"></div>
           </div>
         </div>
-        <button id="displayFavButton" onClick={favOnClick} type="button">
-          Only display favorites
-        </button>
-        <div
-          id="up-arrow"
-          className={open ? "open" : "closed"}
-          onClick={() => setOpen(!open)}
-        ></div>
+        <div id="up-arrow" onClick={() => setOpen(!open)}></div>
+        <div id="blurry-area"></div>
       </div>
       <div
         id="burger-button"
         className={open ? "open" : "closed"}
         onClick={() => setOpen(!open)}
       >
-        <div id="top-line" className={open ? "open" : "closed"}></div>
-        <div id="middle-line" className={open ? "open" : "closed"}></div>
-        <div id="bottom-line" className={open ? "open" : "closed"}></div>
+        <div id="top-line"></div>
+        <div id="middle-line"></div>
+        <div id="bottom-line"></div>
       </div>
-      <div id="blurry-area" className={open ? "open" : "closed"}></div>
     </div>
   );
 };
