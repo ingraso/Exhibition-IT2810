435f42b1f9eb455afa240249e98e884f50f89430
Karolill <56150049+Karolill@users.noreply.github.com>
2020-09-24 14:06:32 +0200

    #28 - apply_btn is greyed out when your filtering removes all installations

--- exhibition/src/components/FilterCheckbox/filterCheckbox.tsx
+++ exhibition/src/components/FilterCheckbox/filterCheckbox.tsx
@@ -2,10 +2,20 @@ import React from "react";
 import {
   InstallationFilter,
   tagIsChosen,
+  applyPossible
 } from "../InstallationFilter/installationFilter";
 
 const filterCheckboxClick = (new_tag: string) => {
   InstallationFilter(new_tag);
+
+  const applyButton = document.getElementById("apply_btn");
+    if(applyPossible()) {
+      applyButton?.classList.add("allowedButton");
+      applyButton?.classList.remove("disabledButton");
+    } else {
+      applyButton?.classList.remove("allowedButton");
+      applyButton?.classList.add("disabledButton");
+    }
 };
 
 export const FilterCheckbox = ({
